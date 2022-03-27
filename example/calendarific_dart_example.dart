39b6946d616b9a6485f5b8d1546c4f72e2ef740d
Markus jayjah <markus@jayjah.de>
2022-03-27 17:05:36 +0200

    added example folder

---
+++ example/calendarific_dart_example.dart
@@ -0,0 +1,9 @@
+import 'package:calendarific_dart/calendarific_dart.dart';
+
+Future<void> main() async {
+  const String apiKey = '';
+  final CalendarificApi api = CalendarificApi(apiKey);
+  print('Countries: ${await api.getCountries()}');
+  print('Languages: ${await api.getLanguages()}');
+  print('Holidays: ${await api.getHolidays(countryCode: 'DE', year: '2022')}');
+}
