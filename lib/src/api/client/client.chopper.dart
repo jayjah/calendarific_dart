f67b4b175ff25f37b5c15976f6fcb957b2bba29b
Markus jayjah <markus@jayjah.de>
2023-03-10 13:30:04 +0100

    update deps && dart version && regenerate code with new deps

--- lib/src/api/client/client.chopper.dart
+++ lib/src/api/client/client.chopper.dart
@@ -18,190 +18,297 @@ class _$CalendarificClient extends CalendarificClient {
 
   @override
   Future<Response<dynamic>> getHolidays(
-      String apiKey, String countryCode, String year) {
-    final $url =
-        'https://calendarific.com/api/v2/holidays?api_key=${apiKey}&country=${countryCode}&year=${year}';
-    final $headers = {
+    String apiKey,
+    String countryCode,
+    String year,
+  ) {
+    final Uri $url = Uri.parse(
+        'https://calendarific.com/api/v2/holidays?api_key=${apiKey}&country=${countryCode}&year=${year}');
+    final Map<String, String> $headers = {
       'Content-Type': 'application/json',
     };
-
-    final $request = Request('GET', $url, client.baseUrl, headers: $headers);
+    final Request $request = Request(
+      'GET',
+      $url,
+      client.baseUrl,
+      headers: $headers,
+    );
     return client.send<dynamic, dynamic>($request);
   }
 
   @override
   Future<Response<dynamic>> getHolidaysByType(
-      String apiKey, String countryCode, String year, String type) {
-    final $url =
-        'https://calendarific.com/api/v2/holidays?api_key=${apiKey}&country=${countryCode}&year=${year}&type=${type}';
-    final $headers = {
+    String apiKey,
+    String countryCode,
+    String year,
+    String type,
+  ) {
+    final Uri $url = Uri.parse(
+        'https://calendarific.com/api/v2/holidays?api_key=${apiKey}&country=${countryCode}&year=${year}&type=${type}');
+    final Map<String, String> $headers = {
       'Content-Type': 'application/json',
     };
-
-    final $request = Request('GET', $url, client.baseUrl, headers: $headers);
+    final Request $request = Request(
+      'GET',
+      $url,
+      client.baseUrl,
+      headers: $headers,
+    );
     return client.send<dynamic, dynamic>($request);
   }
 
   @override
   Future<Response<dynamic>> getHolidaysByLocation(
-      String apiKey, String countryCode, String year, String location) {
-    final $url =
-        'https://calendarific.com/api/v2/holidays?api_key=${apiKey}&country=${countryCode}&year=${year}&location=${location}';
-    final $headers = {
+    String apiKey,
+    String countryCode,
+    String year,
+    String location,
+  ) {
+    final Uri $url = Uri.parse(
+        'https://calendarific.com/api/v2/holidays?api_key=${apiKey}&country=${countryCode}&year=${year}&location=${location}');
+    final Map<String, String> $headers = {
       'Content-Type': 'application/json',
     };
-
-    final $request = Request('GET', $url, client.baseUrl, headers: $headers);
+    final Request $request = Request(
+      'GET',
+      $url,
+      client.baseUrl,
+      headers: $headers,
+    );
     return client.send<dynamic, dynamic>($request);
   }
 
   @override
-  Future<Response<dynamic>> getHolidaysByLocationAndType(String apiKey,
-      String countryCode, String year, String location, String type) {
-    final $url =
-        'https://calendarific.com/api/v2/holidays?api_key=${apiKey}&country=${countryCode}&year=${year}&location=${location},type${type}';
-    final $headers = {
+  Future<Response<dynamic>> getHolidaysByLocationAndType(
+    String apiKey,
+    String countryCode,
+    String year,
+    String location,
+    String type,
+  ) {
+    final Uri $url = Uri.parse(
+        'https://calendarific.com/api/v2/holidays?api_key=${apiKey}&country=${countryCode}&year=${year}&location=${location},type${type}');
+    final Map<String, String> $headers = {
       'Content-Type': 'application/json',
     };
-
-    final $request = Request('GET', $url, client.baseUrl, headers: $headers);
+    final Request $request = Request(
+      'GET',
+      $url,
+      client.baseUrl,
+      headers: $headers,
+    );
     return client.send<dynamic, dynamic>($request);
   }
 
   @override
   Future<Response<dynamic>> getSpecificHolidays(
-      String apiKey, String countryCode, String year, int day, int month) {
-    final $url =
-        'https://calendarific.com/api/v2/holidays?api_key=${apiKey}&country=${countryCode}&year=${year}&day=${day},month=${month}';
-    final $headers = {
+    String apiKey,
+    String countryCode,
+    String year,
+    int day,
+    int month,
+  ) {
+    final Uri $url = Uri.parse(
+        'https://calendarific.com/api/v2/holidays?api_key=${apiKey}&country=${countryCode}&year=${year}&day=${day},month=${month}');
+    final Map<String, String> $headers = {
       'Content-Type': 'application/json',
     };
-
-    final $request = Request('GET', $url, client.baseUrl, headers: $headers);
+    final Request $request = Request(
+      'GET',
+      $url,
+      client.baseUrl,
+      headers: $headers,
+    );
     return client.send<dynamic, dynamic>($request);
   }
 
   @override
-  Future<Response<dynamic>> getSpecificHolidaysByType(String apiKey,
-      String countryCode, String year, int day, int month, String type) {
-    final $url =
-        'https://calendarific.com/api/v2/holidays?api_key=${apiKey}&country=${countryCode}&year=${year}&day=${day},month=${month},type=${type}';
-    final $headers = {
+  Future<Response<dynamic>> getSpecificHolidaysByType(
+    String apiKey,
+    String countryCode,
+    String year,
+    int day,
+    int month,
+    String type,
+  ) {
+    final Uri $url = Uri.parse(
+        'https://calendarific.com/api/v2/holidays?api_key=${apiKey}&country=${countryCode}&year=${year}&day=${day},month=${month},type=${type}');
+    final Map<String, String> $headers = {
       'Content-Type': 'application/json',
     };
-
-    final $request = Request('GET', $url, client.baseUrl, headers: $headers);
+    final Request $request = Request(
+      'GET',
+      $url,
+      client.baseUrl,
+      headers: $headers,
+    );
     return client.send<dynamic, dynamic>($request);
   }
 
   @override
-  Future<Response<dynamic>> getSpecificHolidaysByLocation(String apiKey,
-      String countryCode, String year, int day, int month, String location) {
-    final $url =
-        'https://calendarific.com/api/v2/holidays?api_key=${apiKey}&country=${countryCode}&year=${year}&day=${day},month=${month},location=${location}';
-    final $headers = {
+  Future<Response<dynamic>> getSpecificHolidaysByLocation(
+    String apiKey,
+    String countryCode,
+    String year,
+    int day,
+    int month,
+    String location,
+  ) {
+    final Uri $url = Uri.parse(
+        'https://calendarific.com/api/v2/holidays?api_key=${apiKey}&country=${countryCode}&year=${year}&day=${day},month=${month},location=${location}');
+    final Map<String, String> $headers = {
       'Content-Type': 'application/json',
     };
-
-    final $request = Request('GET', $url, client.baseUrl, headers: $headers);
+    final Request $request = Request(
+      'GET',
+      $url,
+      client.baseUrl,
+      headers: $headers,
+    );
     return client.send<dynamic, dynamic>($request);
   }
 
   @override
   Future<Response<dynamic>> getSpecificHolidaysByLocationAndType(
-      String apiKey,
-      String countryCode,
-      String year,
-      int day,
-      int month,
-      String location,
-      String type) {
-    final $url =
-        'https://calendarific.com/api/v2/holidays?api_key=${apiKey}&country=${countryCode}&year=${year}&day=${day},month=${month},location=${location},type=${type}';
-    final $headers = {
+    String apiKey,
+    String countryCode,
+    String year,
+    int day,
+    int month,
+    String location,
+    String type,
+  ) {
+    final Uri $url = Uri.parse(
+        'https://calendarific.com/api/v2/holidays?api_key=${apiKey}&country=${countryCode}&year=${year}&day=${day},month=${month},location=${location},type=${type}');
+    final Map<String, String> $headers = {
       'Content-Type': 'application/json',
     };
-
-    final $request = Request('GET', $url, client.baseUrl, headers: $headers);
+    final Request $request = Request(
+      'GET',
+      $url,
+      client.baseUrl,
+      headers: $headers,
+    );
     return client.send<dynamic, dynamic>($request);
   }
 
   @override
   Future<Response<dynamic>> getHolidaysFromMonth(
-      String apiKey, String countryCode, String year, int month) {
-    final $url =
-        'https://calendarific.com/api/v2/holidays?api_key=${apiKey}&country=${countryCode}&year=${year}&month=${month}';
-    final $headers = {
+    String apiKey,
+    String countryCode,
+    String year,
+    int month,
+  ) {
+    final Uri $url = Uri.parse(
+        'https://calendarific.com/api/v2/holidays?api_key=${apiKey}&country=${countryCode}&year=${year}&month=${month}');
+    final Map<String, String> $headers = {
       'Content-Type': 'application/json',
     };
-
-    final $request = Request('GET', $url, client.baseUrl, headers: $headers);
+    final Request $request = Request(
+      'GET',
+      $url,
+      client.baseUrl,
+      headers: $headers,
+    );
     return client.send<dynamic, dynamic>($request);
   }
 
   @override
   Future<Response<dynamic>> getHolidaysFromMonthByType(
-      String apiKey, String countryCode, String year, int month, String type) {
-    final $url =
-        'https://calendarific.com/api/v2/holidays?api_key=${apiKey}&country=${countryCode}&year=${year}&month=${month},type${type}';
-    final $headers = {
+    String apiKey,
+    String countryCode,
+    String year,
+    int month,
+    String type,
+  ) {
+    final Uri $url = Uri.parse(
+        'https://calendarific.com/api/v2/holidays?api_key=${apiKey}&country=${countryCode}&year=${year}&month=${month},type${type}');
+    final Map<String, String> $headers = {
       'Content-Type': 'application/json',
     };
-
-    final $request = Request('GET', $url, client.baseUrl, headers: $headers);
+    final Request $request = Request(
+      'GET',
+      $url,
+      client.baseUrl,
+      headers: $headers,
+    );
     return client.send<dynamic, dynamic>($request);
   }
 
   @override
-  Future<Response<dynamic>> getHolidaysFromMonthByLocation(String apiKey,
-      String countryCode, String year, int month, String location) {
-    final $url =
-        'https://calendarific.com/api/v2/holidays?api_key=${apiKey}&country=${countryCode}&year=${year}&month=${month},location=${location}';
-    final $headers = {
+  Future<Response<dynamic>> getHolidaysFromMonthByLocation(
+    String apiKey,
+    String countryCode,
+    String year,
+    int month,
+    String location,
+  ) {
+    final Uri $url = Uri.parse(
+        'https://calendarific.com/api/v2/holidays?api_key=${apiKey}&country=${countryCode}&year=${year}&month=${month},location=${location}');
+    final Map<String, String> $headers = {
       'Content-Type': 'application/json',
     };
-
-    final $request = Request('GET', $url, client.baseUrl, headers: $headers);
+    final Request $request = Request(
+      'GET',
+      $url,
+      client.baseUrl,
+      headers: $headers,
+    );
     return client.send<dynamic, dynamic>($request);
   }
 
   @override
   Future<Response<dynamic>> getHolidaysFromMonthByLocationAndType(
-      String apiKey,
-      String countryCode,
-      String year,
-      int month,
-      String location,
-      String type) {
-    final $url =
-        'https://calendarific.com/api/v2/holidays?api_key=${apiKey}&country=${countryCode}&year=${year}&month=${month},location=${location},type${type}';
-    final $headers = {
+    String apiKey,
+    String countryCode,
+    String year,
+    int month,
+    String location,
+    String type,
+  ) {
+    final Uri $url = Uri.parse(
+        'https://calendarific.com/api/v2/holidays?api_key=${apiKey}&country=${countryCode}&year=${year}&month=${month},location=${location},type${type}');
+    final Map<String, String> $headers = {
       'Content-Type': 'application/json',
     };
-
-    final $request = Request('GET', $url, client.baseUrl, headers: $headers);
+    final Request $request = Request(
+      'GET',
+      $url,
+      client.baseUrl,
+      headers: $headers,
+    );
     return client.send<dynamic, dynamic>($request);
   }
 
   @override
   Future<Response<dynamic>> getLanguages(String apiKey) {
-    final $url = 'https://calendarific.com/api/v2/languages?api_key=${apiKey}';
-    final $headers = {
+    final Uri $url = Uri.parse(
+        'https://calendarific.com/api/v2/languages?api_key=${apiKey}');
+    final Map<String, String> $headers = {
       'Content-Type': 'application/json',
     };
-
-    final $request = Request('GET', $url, client.baseUrl, headers: $headers);
+    final Request $request = Request(
+      'GET',
+      $url,
+      client.baseUrl,
+      headers: $headers,
+    );
     return client.send<dynamic, dynamic>($request);
   }
 
   @override
   Future<Response<dynamic>> getCountries(String apiKey) {
-    final $url = 'https://calendarific.com/api/v2/countries?api_key=${apiKey}';
-    final $headers = {
+    final Uri $url = Uri.parse(
+        'https://calendarific.com/api/v2/countries?api_key=${apiKey}');
+    final Map<String, String> $headers = {
       'Content-Type': 'application/json',
     };
-
-    final $request = Request('GET', $url, client.baseUrl, headers: $headers);
+    final Request $request = Request(
+      'GET',
+      $url,
+      client.baseUrl,
+      headers: $headers,
+    );
     return client.send<dynamic, dynamic>($request);
   }
 }
